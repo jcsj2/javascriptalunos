22ed2fd83a16127aa23b906c958a50832b47fc0d
Jose Claudio da Silva Junior (CESAR) <jcsj2@cesar.org.br>
2022-02-24 18:57:17 -0300

    Add exemplo desafio 1

---
+++ desafio1-example/index.js
@@ -0,0 +1,39 @@
+//https://snyk.io/vuln/npm:jquery@3.4.1
+
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+async function search (packageName, version) {
+    const url = `https://snyk.io/vuln/npm:${encodeURIComponent(packageName)}@${version}`;
+
+    try {
+
+        const { data } = await axios(url)
+        const html = data
+        const $ = cheerio.load(html)
+        
+        if (hasVulnerabilities($)) {
+            console.log(`${packageName} ${version} has vulnerabilities`)
+            $('.table--comfortable', html).each(function () {
+                // console.log($(this).find('tbody').find('tr').text())
+                $(this).find('tbody').find('tr').each(function () {
+                    console.log($(this).find('strong').text() + " - " + $(this).find('.semver').text())
+                })
+            })
+            console.log("======  ======")
+        }
+
+    } catch (err) {
+        console.log(packageName + " error ")
+    }
+
+    
+} 
+
+function hasVulnerabilities(cheerioLoaded) {
+    return !cheerioLoaded.text().includes('No known vulnerabilities have been found for this package in Snyk\'s vulnerability database.')
+}
+
+search('jquery', '3.4.1')
+search('adm-zip', '0.4.9')
+search('adm-zip', '0.5.6')
